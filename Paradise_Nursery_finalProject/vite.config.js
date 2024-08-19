547c9cb7b238093d55e9e113aa66e8a588d7e84e
deva-04 <bandarudeva2003@gmail.com>
2024-08-19 19:43:10 +0530

    modified

--- Paradise_Nursery_finalProject/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
- base: '/Developing-Front-End-Apps-with-React-IBM/Paradise_Nursery_finalProject/', 
-  plugins: [react()],
-})

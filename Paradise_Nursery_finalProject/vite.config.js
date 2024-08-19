5ca96bc83cebecd0850e7e46f37ef5dec75e2c32
Deva Srirama Sai Ganesh B <144914899+deva-04@users.noreply.github.com>
2024-08-19 16:21:36 +0530

    Update vite.config.js

--- Paradise_Nursery_finalProject/vite.config.js
+++ Paradise_Nursery_finalProject/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   //base: "/shoppingreact",
-  base: "/deva-04",
+  //base: "/deva-04",
+  base: "/Developing-Front-End-Apps-with-React-IBM/Paradise_Nursery_finalProject/",
   plugins: [react()],
 })
